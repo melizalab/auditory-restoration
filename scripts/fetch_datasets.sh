4fa7a5a3d9d98cdc8c48bf958de56b7552a833f1
Bao Le <uac6qw@virginia.edu>
2025-11-07 21:04:38 -0500

    updated fetch_datasets bash script

--- scripts/fetch_datasets.sh
+++ scripts/fetch_datasets.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/env bash
 set -e
-OUTDIR="datasets"
+DSDIR="datasets"
+BUILDDIR="build"
+OUTDIR="output"
+
+if [ ! -d ${DSDIR} ]; then
+    mkdir ${DSDIR}
+fi
+
+if [ ! -d ${BUILDDIR}]; then
+    mkdir ${BUILDDIR}
+fi
+
+if [ ! -d ${OUTDIR}]; then
+    mkdir ${OUTDIR}
+fi
 
 # retrieve the datasets
-curl -o ${OUTDIR}/zebf-auditory-restoration-1.zip https://figshare.com/ndownloader/files/55083911
+curl -o ${DSDIR}/zebf-auditory-restoration-1.zip https://figshare.com/ndownloader/files/55083911
 
-# unpack the datasets
-for zipfile in ${OUTDIR}/*.zip; do
-    if [ -f "$zipfile" ] ; then
-	unzip -o ${zipfile} -d ${OUTDIR}
-    fi
-done
+# unpack the dataset
+unzip -o ${DSDIR}/zebf-auditory-restoration-1.zip -d ${DSDIR}
+mv ${DSDIR}/zebf-auditory-restoration-1/* ${DSDIR}/
 
-# delete the zip files
-rm ${OUTDIR}/*.zip
+# delete archive folder and zip file
+rm -r ${DSDIR}/zebf-auditory-restoration-1 ${DSDIR}/*.zip
